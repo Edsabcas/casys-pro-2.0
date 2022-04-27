9dc5fc7e7cd5423b5f100c98ca77a4acd8c8c89d
ByronIguardia18 <iguardiae@gmail.com>
2022-04-27 15:49:26 -0600

    Cambios en formulario de grados

--- resources/views/Grados/formulario.blade.php
+++ resources/views/Grados/formulario.blade.php
@@ -13,45 +13,47 @@
   @endif
 @endisset
 
-<form wire:submit.prevent="">
+<form row g-3 wire:submit.prevent="">
     @csrf
-    <div class="mb-3">
-      <label for="floatingInput">Ingresar Grado:</label>
-      <input type="text" class="form-control" id="floatingInput"  wire:model="nombre_gr" required>
+    <div class="container">
+      <div class="row">
+      <div class="mb-3 col-md-4 ">
+          <label for="floatingInput">Ingresar Grado:</label>
+          <input type="text" class="form-control" id="floatingInput"  wire:model="nombre_gr" required>
+        </div>
+      </div>
     </div>
     @error('nombre_gr') 
-    <div class="alert alert-danger d-flex align-items-center" role="alert">
-      <svg class="bi flex-shrink-0 me-2" width="24" height="24" role="img" aria-label="Success:"><use xlink:href="#check-circle-fill"/></svg>
-      <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-exclamation-triangle-fill flex-shrink-0 me-2" viewBox="0 0 16 16" role="img" aria-label="Warning:">
-        <path d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5zm.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
-      </svg>
-        <span>Pendiente el ingreso del grado</span>
-       </div> 
-       @enderror
-       <div class="mb-3">
-        <label for="exampleInputEmail1" class="form-label">Sección:</label>
-        <select class="form-select input-group mb-3" aria-label="Default select example" wire:model="seccion_gr">
-          <option selected>Seleccionar:</option>
-          @isset($secciones)
+      <div class="alert alert-danger d-flex align-items-center" role="alert">
+        <svg class="bi flex-shrink-0 me-2" width="24" height="24" role="img" aria-label="Success:"><use xlink:href="#check-circle-fill"/></svg>
+        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-exclamation-triangle-fill flex-shrink-0 me-2" viewBox="0 0 16 16" role="img" aria-label="Warning:">
+          <path d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5zm.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
+        </svg>
+          <span>Pendiente el ingreso del grado</span>
+        </div> 
+    @enderror
+      <div class="container">
+        <div class="input-group col-md-4 offset-md-4">
+          <button class="btn btn-outline-primary" data-bs-toggle="modal" data-bs-target="#staticBackdrop" type="button"><img src="https://img.icons8.com/material-two-tone/24/000000/add.png"/></button>
+          <select class="form-select" id="inputGroupSelect03" aria-label="Example select with button addon" wire:model="seccion_gr">
+            <option selected>Elegir Sección:</option>
+            @isset($secciones)
             @foreach ($secciones as $seccion)
               <option value="{{$seccion->ID_SC}}">{{$seccion->SECCION}}</option>
             @endforeach              
           @endisset
-        </select>
-      </div>
+          </select>
+        </div>
+      </div>  
       @error('seccion_gr') 
-    <div class="alert alert-danger d-flex align-items-center" role="alert">
-      <svg class="bi flex-shrink-0 me-2" width="24" height="24" role="img" aria-label="Success:"><use xlink:href="#check-circle-fill"/></svg>
-      <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-exclamation-triangle-fill flex-shrink-0 me-2" viewBox="0 0 16 16" role="img" aria-label="Warning:">
-        <path d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5zm.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
-      </svg>
-        <span>Pendiente de asignar la sección</span>
-       </div> 
-       @enderror
-       <!-- Button trigger modal -->
-      <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#staticBackdrop">
-        Siguiente
-      </button>
+        <div class="alert alert-danger d-flex align-items-center" role="alert">
+          <svg class="bi flex-shrink-0 me-2" width="24" height="24" role="img" aria-label="Success:"><use xlink:href="#check-circle-fill"/></svg>
+          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-exclamation-triangle-fill flex-shrink-0 me-2" viewBox="0 0 16 16" role="img" aria-label="Warning:">
+            <path d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5zm.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
+          </svg>
+            <span>Pendiente de asignar la sección</span>
+        </div> 
+      @enderror
 
       <!-- Modal -->
       <div wire:ignore.self class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
@@ -102,7 +104,7 @@
       </div>
       <br>
       
-         <div class="mb-3">
+         <div class="mb-3 col-auto">
           <label for="floatingInput">Ingresar Nombre Ministerial:</label>
           <input type="text" class="form-control" id="floatingInput"  wire:model="ministerial_gr" required>
         </div> 
@@ -129,7 +131,7 @@
             <span>Pendiente el ingresar el numero de resolución</span>
            </div> 
            @enderror
-           <div class="mb-3">
+           <div class="mb-3 col-auto">
             <label for="floatingInput">Ingresar Nivel Académico:</label>
             <select class="form-select" aria-label="Default select example" wire:model="academico_gr">
               <option selected>Seleccionar:</option>
