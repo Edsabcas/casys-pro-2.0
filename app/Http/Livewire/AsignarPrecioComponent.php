83d435f54015742bd51a0ee894b6876f40a4af37
edgsabcas <edgar.casuy123@gmail.com>
2022-05-03 23:11:27 -0600

    cambios para precios de grados

--- app/Http/Livewire/AsignarPrecioComponent.php
+++ app/Http/Livewire/AsignarPrecioComponent.php
@@ -7,18 +7,27 @@ use Illuminate\Support\Facades\DB;
 
 class AsignarPrecioComponent extends Component
 {
-    public $id_grado,$id_pro,$monto,$mensaje,$mensaje1;
+    public $id_grado,$id_pro,$monto,$mensaje,$mensaje1,$id_pre_gra;
     public function render()
     {
         $sql= 'SELECT * FROM tb_grados where ESTADO=1';
         $tb_grados=DB::select($sql);
         $sql= 'SELECT * FROM TB_PROCESO_PAGO where ESTADO=1';
         $tb_procesos=DB::select($sql);
-        $sql= 'SELECT * FROM TB_PRECIOS_GRADOS_INS where ID_PRO_PAGO=1';
+        $sql= 'SELECT GRA.GRADO, PRE.MONTO,PRE.ID_GRADO, PRE.ID_PRO_PAGO,PRE.ID_PRE_GRA_INS, PROC.DESCRIPCION FROM tb_grados GRA
+        INNER JOIN TB_PRECIOS_GRADOS_INS PRE ON
+        GRA.ID_GR=PRE.ID_GRADO INNER JOIN TB_PROCESO_PAGO  PROC ON
+        PRE.ID_PRO_PAGO=PROC.ID_PROCESO_P WHERE PRE.ID_PRO_PAGO=1';
         $tb_precios_g=DB::select($sql);
-        $sql= 'SELECT * FROM TB_PRECIOS_GRADOS_INS where ID_PRO_PAGO=2';
+        $sql= 'SELECT GRA.GRADO, PRE.MONTO,PRE.ID_GRADO, PRE.ID_PRE_GRA_INS, PRE.ID_PRO_PAGO, PROC.DESCRIPCION FROM tb_grados GRA
+        INNER JOIN TB_PRECIOS_GRADOS_INS PRE ON
+        GRA.ID_GR=PRE.ID_GRADO INNER JOIN TB_PROCESO_PAGO  PROC ON
+        PRE.ID_PRO_PAGO=PROC.ID_PROCESO_P WHERE PRE.ID_PRO_PAGO=2';
         $tb_precios_ins=DB::select($sql);
-        $sql= 'SELECT * FROM TB_PRECIOS_GRADOS_INS where ID_PRO_PAGO=3';
+        $sql= 'SELECT GRA.GRADO, PRE.MONTO, PRE.ID_GRADO, PRE.ID_PRE_GRA_INS, PRE.ID_PRO_PAGO, PROC.DESCRIPCION FROM tb_grados GRA
+        INNER JOIN TB_PRECIOS_GRADOS_INS PRE ON
+        GRA.ID_GR=PRE.ID_GRADO INNER JOIN TB_PROCESO_PAGO  PROC ON
+        PRE.ID_PRO_PAGO=PROC.ID_PROCESO_P WHERE PRE.ID_PRO_PAGO=3';
         $tb_precios_rec=DB::select($sql);
         
 
@@ -38,7 +47,13 @@ class AsignarPrecioComponent extends Component
         }
         else{
 
+            $sql= 'SELECT * FROM TB_PRECIOS_GRADOS_INS where ID_GRADO=? and ID_PRO_PAGO=?';
+        $valprp=DB::select($sql,array($this->id_grado,$this->id_pro));
 
+        if($valprp!=null){
+            $this->mensaje=4;
+        }
+        else{
             DB::beginTransaction();
             if( DB::table('TB_PRECIOS_GRADOS_INS')->insert(
                 ['ID_GRADO' => $this->id_grado,
@@ -50,15 +65,79 @@ class AsignarPrecioComponent extends Component
               ]))
               {
                 DB::commit();
+              
+                $this->mensaje=1;
                 $this->reset();
                 unset($this->mensaje1);
-                $this->mensaje=1;
               }
             else{
                 DB::rollback();
                 session(['error' => 'validar']);
                 unset($this->mensaje);
                 $this->mensaje1=1;
+            }
+        }
+
+        }
+    }
+    public function edit($id,$id_gra,$tipo_pago,$monto)
+    {
+
+        $this->limpiar();
+        $this->id_grado=$id_gra;
+        $this->id_pro=$tipo_pago;
+        $this->monto=$monto;
+        $this->id_pre_gra=$id;
+
+
+    }
+    public function limpiar() {
+        $this->reset();
+    }
+    public function actualizar() {
+        if($this->validate([
+            'id_grado' => 'required',
+            'id_pro' => 'required',
+            'monto' => 'required',
+        ])==false){
+           // $advertencia="no encontrado";
+            session(['message'=>'no encontrado']);
+            return back()->withErrors(['advertencia'=>'validar el input vacío']);
+
+        }
+        else{
+            $sql= 'SELECT * FROM TB_PRECIOS_GRADOS_INS where ID_GRADO=? and ID_PRO_PAGO=?';
+            $valprp=DB::select($sql,array($this->id_grado,$this->id_pro));
+    
+            if($valprp!=null){
+                $this->mensaje=4;
+            }
+            else{
+                DB::beginTransaction();
+                if(DB::table('TB_PRECIOS_GRADOS_INS')
+                ->where('ID_PRO_PAGO', $this->id_pre_gra)
+                ->update(
+                    [
+                        'ID_GRADO' => $this->id_grado,
+                        'ID_PRO_PAGO' => $this->id_pro,
+                        'MONTO'=> $this->monto,
+                        'FECHA_ACTULIZACION'=>date('Y-m-d H:i:s'),
+    
+                    ]))
+                    
+                  {
+                    DB::commit();
+                    $this->reset();
+                    $this->mensaje=2;
+                    unset($this->mensaje1);
+                  }
+                else{
+                    DB::rollback();
+                    session(['error' => 'validar']);
+                    unset($this->mensaje);
+                    $this->mensaje1=3;
+                }
+
             }
         }
     }
