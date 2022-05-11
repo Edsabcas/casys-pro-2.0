f6eb419bb29afef9cf5336b7337624d25de7193c
Christopher Navas <christophernavas2004@gmail.com>
2022-05-11 10:47:59 -0600

    Cambios Grupo 3

--- resources/views/Unidades/Actividades.blade.php
+++ resources/views/Unidades/Actividades.blade.php
@@ -3,20 +3,23 @@
       <thead>
         
 
-        @foreach ($actividad as $activi)
+        
           <tr>
               <th>Alumno</th>
-              <th>{{$activi->ID_ACTIVIDADES}}</th>
+              @foreach ($actividad as $activi)
+              <th>Actividad {{$activi->ID_ACTIVIDADES}}</th>
+              @endforeach
           </tr>
         
         
       </thead>
-      @endforeach
+     
       <tbody>
         @foreach($estu as $est)
         <tr>
-          @foreach($actividad as $activi)
+          
           <th>{{$est->TB_INFO_NOMBRE}}</th> 
+          @foreach($actividad as $activi)
           <th><input type="text" class="form-control" id="exampleForm{{$est->TB_INFO_NOMBRE}}" placeholder="Nota" wire:click='nota("{{$activi->ID_ACTIVIDADES}}")'></th>
           @endforeach
           
