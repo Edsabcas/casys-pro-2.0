12f36d970a133de2d63ccbb283e2c0c67a3cbf25
Jimena Gómez <jimenalejandra0292@gmail.com>
2022-05-10 19:20:22 -0600

    NUEVO DISEÑO login

--- resources/views/auth/hola.blade.php
+++ resources/views/auth/hola.blade.php
@@ -10,6 +10,7 @@
     <meta name="author" content="">
 
     <title> CASYS </title>
+    <link rel="icon" href="{{ asset('img/lo.png')}}" />
 
     <!-- Custom fonts for this template-->
     <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
@@ -20,38 +21,39 @@
     <!-- Custom styles for this template-->
     <link href="css/sb-admin-2.min.css" rel="stylesheet">
 
+    
 </head>
 
-<body class="bg-gradient-primary">
+<body class="bg-gradient" style="background-color: #a4cb39; font-family: Century Gothic">
 
     
 
     <div class="row justify-content-center">
 
-        <div class="col-xl-10 col-lg-12 col-md-9">
+        <div class="col-xl-10 col-lg-12 col-md-10">
     
             <div class="card o-hidden border-0 shadow-lg my-5">
                 <div class="card-body p-0">
                     <!-- Nested Row within Card Body -->
                     <div class="row">
-                        <div class="col-lg-6 d-none d-lg-block">
-                            <img src="https://scontent.fgua3-4.fna.fbcdn.net/v/t1.6435-9/86498843_608790926346868_7368970768085942272_n.jpg?_nc_cat=111&ccb=1-5&_nc_sid=0debeb&_nc_ohc=_V5tgzPwEhcAX8SV-IC&_nc_ht=scontent.fgua3-4.fna&oh=00_AT8VKJ_WXwNP6d_5idtvic2ksvXV5kYQJLctlbjYJNXbfw&oe=6270C593" class="rounded mx-auto d-block" width="500" height="415" alt="...">
+                        <div class="col-lg-5 d-none d-lg-block">
+                            <img src="img/lo.png" class="rounded mx-auto d-block" width="450" height="445" alt="...">
                         </div>
                         <div class="col-lg-6">
                             <div class="p-5">
                                 <div class="text-center">
-                                    <h1 class="h4 text-gray-900 mb-4">Bienvenido</h1>
+                                    <h1 class="text-center" style="color:#3a3e7b"><strong>¡BIENVENIDO!</strong></h1>
+                                    <h5 class="text-center" style="color: black">Ingresa con tu usuario y contraseña.</h5>
                                 </div>
 
-
-                                    
 
                                 <form class="mt-4" method="POST" action="/ingresar" >
                                     @csrf
                                     <div class="form-floating mb-3">
-                                        <input type="text" class="form-control" id="floatingInput" placeholder="Usuario" name="usuario">
-                                        <label for="floatingInput">Usuario</label>
+                                        <input type="text" style="border-color: #a4cb39" class="form-control rounded-pill" id="floatingInput" placeholder="Usuario" name="usuario">
+                                        <span></span>
                                       </div>
+                                      <br>
                                       @error('usuario')
                                       <div class="alert alert-danger d-flex align-items-center" role="alert">
                                       <svg class="bi flex-shrink-0 me-2" width="24" height="24" role="img" aria-label="Success:"><use xlink:href="#check-circle-fill"/></svg>
@@ -60,8 +62,7 @@
                                       @enderror
 
                                       <div class="form-floating">
-                                        <input type="password" class="form-control" id="floatingPassword" name="password" placeholder="Password">
-                                        <label for="floatingPassword">Contraseña</label>
+                                        <input type="password" style="border-color: #a4cb39" class="form-control rounded-pill" id="floatingPassword" name="password" placeholder="Contraseña"><i class="bi bi-key-fill" style="color: black"></i>
                                       </div>
                                       @error('password')
                                       <div class="alert alert-danger d-flex align-items-center" role="alert">
@@ -70,10 +71,9 @@
                                       </div>
                                       @enderror
                             </div>
-                                <hr>
                                 
-                                <button type="submit" class="btn btn-primary btn-user btn-block">
-                                    Ingresar
+                                <button type="submit" class="btn btn-user btn-block text-light rounded-pill" style="background-color:#3a3e7b">
+                                    <b>Ingresar</b>
                                 </button>
                                 @isset($mensaje20)
                                 @if($mensaje20 != null)
