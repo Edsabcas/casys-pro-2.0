5122baf799c6a7c7ef3c357d7e305c89c7d3fa30
edgsabcas <edgar.casuy123@gmail.com>
2022-05-12 22:37:46 -0600

    cambio en menu

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -60,9 +60,9 @@
                 <li class="nav-item">
                     <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseUtilities{{$menu_rol->ID_MENU}}"
                         aria-expanded="true" aria-controls="collapseUtilities{{$menu_rol->ID_MENU}}">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-right-circle-fill" viewBox="0 0 16 16">
-                            <path d="M8 0a8 8 0 1 1 0 16A8 8 0 0 1 8 0zM4.5 7.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5H4.5z"/>
-                          </svg>
+                        @php
+                        echo $menu_rol->ICONO;
+                        @endphp
                         <span>{{$menu_rol->DESCRIPCION}}</span>
                     </a>
                  
