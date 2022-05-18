c6dbe3ca04af44685372a5331c3cff8da9da1e04
Fabian Andres <deflorandelcids@gmail.com>
2022-05-18 14:55:43 -0600

    Cambios grupo 1

--- resources/views/auth/configperfil.blade.php
+++ resources/views/auth/configperfil.blade.php
@@ -25,7 +25,12 @@
                 @endif
                 @endif
           @endforeach       
-            <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#perfilmodal2">Cambiar</button> 
+            <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#perfilmodal2">
+              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16">
+                <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
+                <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"/>
+              </svg>  
+            </button> 
             @if($mensaje24 != null)
             <div class="alert alert-success d-flex align-items-center" role="alert">
             <svg class="bi flex-shrink-0 me-2" width="24" height="24" role="img" aria-label="alert:"><use xlink:href="#exclamation-triangle-fill"/></svg>
@@ -42,10 +47,14 @@
             </div>
          @endif
         </TR>
-        <TR ALIGN=CENTER><TH>EDITAR</TH>
+        <TR ALIGN=CENTER><TH>CONTRASEÑA</TH>
           <TD>
             <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#perfilmodal"> 
-              Contraseña
+              Editar
+              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16">
+                <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
+                <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"/>
+              </svg>
             </button>   
           </TD>
         </TR>
