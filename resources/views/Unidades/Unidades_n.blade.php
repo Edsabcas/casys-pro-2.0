327a23c2c71a9f7910871db5808e412d24601439
unknown <Javier652005@gmail.com>
2022-05-29 22:18:59 -0600

    temas_n

--- resources/views/Unidades/Unidades_n.blade.php
+++ resources/views/Unidades/Unidades_n.blade.php
@@ -5,10 +5,10 @@
         <p class="card-text">Maestro: {{$ID_DOCENTE}}</p>
         <td>
           @include('Unidades.Temas.modal_tem_n')
-          <button class="btn btn-success" data-bs-toggle="modal" data-bs-target="#tema_n" id=tema> Temas </button>
+          <button class="btn btn-success" data-bs-toggle="modal" data-bs-target="#tema_n" id=tema>Temas </button>
   
           @include('Unidades.ActividadesN.modal_actividades_n')
-          <button class="btn btn-success"  data-bs-toggle="modal" data-bs-target="#modal_act_n"> Actividades </button>
+          <button class="btn btn-success"  data-bs-toggle="modal" data-bs-target="#modal_act_n">Crear Actividades </button>
             <a wire:click='vista_a("3")' class="btn btn-success">Ver Actividades </a>
 
             <a wire:click='vista_t("4")' class="btn btn-success">Ver Temas </a>
@@ -16,26 +16,6 @@
         <br>
         <br>
         <br>
-        @include('Unidades.Actividades.Actividades')
+        @include('Unidades.ActividadesN.Act_n')
       </div>
     </div>
-
-
-<div class="card ">
-    <h6>{{$nombreu}}</h6>
-        <div class="card-body  ">
-          <h5 class="card-title">Materia:{{$NOMBRE_MATERIA}}</h5>
-          <p class="card-text">Maestro: {{$ID_DOCENTE}}</p>
-          <td>
-            @include('Unidades.Temas.modal_tem_n')
-            <button class="btn btn-success" data-bs-toggle="modal" data-bs-target="#tema_n" id=tema> Temas </button>
-    
-              @include('Unidades.ActividadesN.modal_actividades_n')
-              <button class="btn btn-success"  data-bs-toggle="modal" data-bs-target="#modal_act_n"> Actividades </button>
-          </td>
-          <br>
-          <br>
-          <br>
-          @include('Unidades.Actividades.Act_n')
-        </div>
-      </div>
