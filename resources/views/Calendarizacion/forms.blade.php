00733b44c41a3f6da7a16a94d5b5de2bf2841f91
Christopher Navas <christophernavas2004@gmail.com>
2022-06-04 19:43:16 -0600

    Cmabios Calendarizacion

--- resources/views/Calendarizacion/forms.blade.php
+++ resources/views/Calendarizacion/forms.blade.php
@@ -48,6 +48,7 @@ Agregado correctamente
 
 <form wire:submit.prevent=''>
     @csrf 
+    @if($op3==null)
       <div class="mb-3">
         <label for="exampleInputEmail1" class="form-label"> Unidad</label>
                 <select class="form-select form-select-sm" aria-label=".form-select-sm example" wire:model='unidad'>
@@ -58,13 +59,25 @@ Agregado correctamente
           @endforeach
           @endisset
         </select>
-        @error('unidad') 
-        <div class="alert alert-danger d-flex align-items-center" role="alert">
-          <svg class="bi flex-shrink-0 me-2" width="24" height="24" role="img" aria-label="Success:"><use xlink:href="#check-circle-fill"/></svg>
-          <span>Pendiente de seleccionar una Unidad</span>
-          </div> 
-        @enderror
       </div>
+      @endif
+      <div class="form-check form-switch form-check-inline">
+        <input class="form-check-input" type="checkbox" id="solicitud" wire:click="validarext('1')" >
+        <label class="form-check-label" for="flexSwitchCheckChecked">Seleccionar una unidad extra</label> 
+      </div>
+      @if($op3==1)
+      <div class="mb-3">
+        <label for="exampleInputEmail1" class="form-label"> Unidad Extra</label>
+                <select class="form-select form-select-sm" aria-label=".form-select-sm example" wire:model='unidadextra'>
+          <option selected>Seleccione la unidad</option>
+          @isset($unidadesex)
+          @foreach ($unidadesex as $unidades)
+              <option value="{{$unidades->ID_UNIDADES}}">{{$unidades->NOMNBRE_UNIDAD}}</option>
+          @endforeach
+          @endisset
+        </select>
+      </div>
+      @endif
        <div class="mb-3">
         <label for="examplenombre" class="form-label"> Fecha de Inicio</label>
         <input type="date" class="form-control" id="name"  wire:model='fecha_ini'>
