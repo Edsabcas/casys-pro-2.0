80f99124e32eb95a43c272a256569ae642484123
Christopher Navas <christophernavas2004@gmail.com>
2022-06-04 19:09:24 -0600

    Cambios Unidades

--- resources/views/Unidades/Unidades_n.blade.php
+++ resources/views/Unidades/Unidades_n.blade.php
@@ -8,17 +8,11 @@
           <button class="btn btn-success" wire:click='limpiar2()' data-bs-toggle="modal" data-bs-target="#tema_n" id=tema>Temas </button>
   
           @include('Unidades.ActividadesN.modal_actividades_n')
-<<<<<<< HEAD
-          <button class="btn btn-success"  data-bs-toggle="modal" data-bs-target="#modal_act_n">Crear Actividades </button>
-            <a wire:click='vista_a("4")' class="btn btn-success">Ver Actividades </a>
 
-            <a wire:click='vista_t("5")' class="btn btn-success">Ver Temas </a>
-=======
           <button class="btn btn-success"  data-bs-toggle="modal" data-bs-target="#modal_act_n" Wire:Click="limpiar_act2">Crear Actividades </button>
             <a wire:click='vista_a("5")' class="btn btn-success">Ver Actividades </a>
 
             <a wire:click='vista_t("6")' class="btn btn-success">Ver Temas </a>
->>>>>>> 0f555d96e55de5e2a8dcb7038d0be501f4406976
         </td>
         <br>
         <br>
