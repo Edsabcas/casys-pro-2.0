1da8cbfa61057d14ba35741357ca82328e845a34
diegocalderon26 <you@example.com>
2022-06-04 19:37:43 -0600

    cambios

--- resources/views/Unidades/Unidades_n.blade.php
+++ resources/views/Unidades/Unidades_n.blade.php
@@ -3,7 +3,20 @@
   <div class="card-body  ">
         <h5 class="card-title">Materia:{{$NOMBRE_MATERIA}}</h5>
         <p class="card-text">Maestro: {{$ID_DOCENTE}}</p>
+          @isset($restriccion)   
         <td>
+          @if($restriccion==1)
+          @include('Unidades.Temas.modal_tem_n')
+          <button class="btn btn-success" wire:click='limpiar2()' data-bs-toggle="modal" data-bs-target="#tema_n" id=tema disabled>Temas </button>
+  
+          @include('Unidades.ActividadesN.modal_actividades_n')
+
+          <button class="btn btn-success"  data-bs-toggle="modal" data-bs-target="#modal_act_n" Wire:Click="limpiar_act2" disabled>Crear Actividades </button>
+            <a wire:click='vista_a("5")' class="btn btn-success">Ver Actividades </a>
+
+            <a wire:click='vista_t("6")' class="btn btn-success">Ver Temas </a>
+          </td>
+          @elseif($restriccion==0)
           @include('Unidades.Temas.modal_tem_n')
           <button class="btn btn-success" wire:click='limpiar2()' data-bs-toggle="modal" data-bs-target="#tema_n" id=tema>Temas </button>
   
@@ -14,6 +27,8 @@
 
             <a wire:click='vista_t("6")' class="btn btn-success">Ver Temas </a>
         </td>
+        @endif
+        @endisset
         <br>
         <br>
         <br>
