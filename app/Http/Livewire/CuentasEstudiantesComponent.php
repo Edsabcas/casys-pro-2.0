de50937edf0e88de70c13fe0895c695cb3f74a1e
ByronIguardia18 <iguardiae@gmail.com>
2022-06-15 09:40:07 -0600

    cambios

--- app/Http/Livewire/CuentasEstudiantesComponent.php
+++ app/Http/Livewire/CuentasEstudiantesComponent.php
@@ -4,14 +4,87 @@ namespace App\Http\Livewire;
 
 use Livewire\Component;
 use Illuminate\Support\Facades\DB;
+use App\Http\Livewire\Request;
 
 class CuentasEstudiantesComponent extends Component
 {
+    public $preinscripcion,$nvlacademico,$grado,$mes,$fpago,$pagor,$montoins,$montomen,$montorecu,$montodes,$mensaje,$mensaje1;
+
     public function render()
     {
         $sql= 'SELECT * FROM cuentaestudiante';
         $cuentas=DB::select($sql);
+        $sql="SELECT * FROM TB_PRE_INS";
+        $inscripciones=DB::select($sql);
+        $sql="SELECT * FROM tb_grados";
+        $grados=DB::select($sql);
+        $sql="SELECT * FROM mes";
+        $meses=DB::select($sql);
+        $sql="SELECT * FROM tb_nvlacademico";
+        $academicos=DB::select($sql);
         
-        return view('livewire.cuentas-estudiantes-component',compact('cuentas'));
+        return view('livewire.cuentas-estudiantes-component',compact('cuentas','academicos','inscripciones','grados','meses'));
+    }
+    public function guardar_cuenta(){
+
+        if($this->validate([
+            'preinscripcion' => 'required',
+            'nvlacademico' => 'required',
+            'grado' => 'required',
+            'mes' => 'required',
+            'fpago' => 'required',
+            'pagor' => 'required',
+            'montoins' => 'required',
+            'montomen' => 'required',
+            'montorecu' => 'required',
+            'montodes' => 'required',
+
+        ])==false)
+        {
+            $mensaje="no encontrado";
+            session(['message' => 'no encontrado']);
+            return back()->withErrors(['mensaje' =>'Validar el input vacio']);
+        }
+        else
+        {
+        $preinscripcion=$this->preinscripcion; 
+        $nvlacademico=$this->nvlacademico;
+        $grado=$this->grado; 
+        $mes=$this->mes;
+        $fpago=$this->fpago;
+        $pagor=$this->pagor; 
+        $montoins=$this->montoins;
+        $montomen=$this->montomen;
+        $montorecu=$this->montorecu;
+        $montodes=$this->montodes;
+
+        DB::beginTransaction();
+
+        $cuentas=DB::table('cuentaestudiante')->insert(
+            [
+                'ID_PRE'=> $preinscripcion,
+                'ID_NVL'=> $nvlacademico,
+                'ID_GR'=> $grado,
+                'ID_MES'=> $mes,
+                'FECHA_PAGO'=> $fpago,
+                'FECHA_ULIMOPAGO'=> $pagor,
+                'MONTO_INSCRIPCION'=> $montoins,
+                'MONTO_RECUPERACION'=> $montomen,
+                'MONTO_MENSUAL'=> $montorecu,
+                'MONTO_DESCUENTO'=> $montodes,
+                'ESTADO'=> $estado=1,
+
+            ]);
+            if($cuentas){
+                DB::commit();
+                $this->reset();
+                $this->mensaje='Insertado correctamente';
+            }
+            else{
+                DB::rollback();
+                unset($this->mensaje);
+                $this->mensaje1='No fue posible insertar correctamente';
+            }
+        }
     }
 }
--- resources/views/cuentas/cuentasestudiantes.blade.php
+++ resources/views/cuentas/cuentasestudiantes.blade.php
@@ -7,80 +7,103 @@
   </div>
         <br><br>
 
-  <form row g-3 wire:submit.prevent="">
-    @csrf
+          <form row g-3 wire:submit.prevent="">
+            @csrf
             <div class="container">
                 <div class="row">
                     <div class="col">
-                        <div class="mb-3">
-                            <label for="floatingInput">Pre-inscripción::</label>
-                            <input type="text" class="form-control" id="floatingInput"  wire:model="preinscripcion" required>
-                        </div>
+                      <label for="recipient-name" class="col-form-label">Pre-inscripción::</label>
+                            <select class="form-select" id="inputGroupSelect03" aria-label="Example select with button addon" wire:model="preinscripcion">
+                              <option selected>Seleccionar:</option>
+                              @isset($inscripciones)
+                              @foreach ($inscripciones as $ins)
+                                <option value="{{$ins->ID_PRE}}">{{$ins->NOMBRE_ES}}</option>
+                              @endforeach              
+                            @endisset
+                            </select>
                     </div>                   
                     <div class="col">
-                      <div class="mb-3">
                         <label for="recipient-name" class="col-form-label">Nivel académico:</label>
-                        <input type="text" class="form-control" wire:model='nvlacademico' id="recipient-name">
-                      </div>
+                        <select class="form-select" id="inputGroupSelect03" aria-label="Example select with button addon" wire:model="nvlacademico">
+                          <option selected>Seleccionar:</option>
+                          @isset($academicos)
+                          @foreach ($academicos as $academico)
+                            <option value="{{$academico->ID_NVL}}">{{$academico->NIVEL_ACADEMICO}}</option>
+                          @endforeach              
+                        @endisset
+                        </select>
                     </div>
-                    <div class="col">
-                        <div class="mb-3">
-                          <label for="recipient-name" class="col-form-label">Grado:</label>
-                          <input type="text" class="form-control" wire:model='grado' id="recipient-name">
-                        </div>
-                      </div>
-                      <div class="col">
-                        <div class="mb-3">
-                          <label for="recipient-name" class="col-form-label">Mes:</label>
-                          <input type="text" class="form-control" wire:model='mes' id="recipient-name">
-                        </div>
-                      </div>
-                      <div class="col">
-                        <div class="mb-3">
-                          <label for="recipient-name" class="col-form-label">Fecha de pago:</label>
-                          <input type="text" class="form-control" wire:model='fpago' id="recipient-name">
-                        </div>
-                      </div>
-                      <div class="col">
-                        <div class="mb-3">
-                          <label for="recipient-name" class="col-form-label">Pago más reciente:</label>
-                          <input type="text" class="form-control" wire:model='pagor' id="recipient-name">
-                        </div>
-                      </div>
-                      <div class="col">
-                        <div class="mb-3">
-                          <label for="recipient-name" class="col-form-label">Monto de inscripción:</label>
-                          <input type="text" class="form-control" wire:model='montoins' id="recipient-name">
-                        </div>
-                      </div>
-                      <div class="col">
-                        <div class="mb-3">
-                          <label for="recipient-name" class="col-form-label">Monto de recuperación:</label>
-                          <input type="text" class="form-control" wire:model='montorecu' id="recipient-name">
-                        </div>
-                      </div>
-                      <div class="col">
-                        <div class="mb-3">
-                          <label for="recipient-name" class="col-form-label">Monto mensual:</label>
-                          <input type="text" class="form-control" wire:model='montomen' id="recipient-name">
-                        </div>
-                      </div>
-                      <div class="col">
-                        <div class="mb-3">
-                          <label for="recipient-name" class="col-form-label">Monto de descuento:</label>
-                          <input type="text" class="form-control" wire:model='' id="recipient-name">
-                        </div>
-                      </div>
+                  <div class="col">
+                      <label for="recipient-name" class="col-form-label">Grado:</label>
+                      <select class="form-select" id="inputGroupSelect03" aria-label="Example select with button addon" wire:model="grado">
+                        <option selected>Seleccionar:</option>
+                        @isset($grados)
+                          @foreach ($grados as $grado)
+                            <option value="{{$grado->ID_GR}}">{{$grado->GRADO}}</option>
+                          @endforeach              
+                        @endisset
+                      </select>
+                  </div>
+                
+                <div class="col">
+                    <label for="recipient-name" class="col-form-label">Mes:</label>
+                    <select class="form-select" id="inputGroupSelect03" aria-label="Example select with button addon" wire:model="mes">
+                      <option selected>Seleccionar:</option>
+                      @isset($meses)
+                        @foreach ($meses as $mes)
+                          <option value="{{$mes->ID_MES}}">{{$mes->DESCRIPCION}}</option>
+                        @endforeach              
+                      @endisset
+                    </select>
                 </div>
-              </form>
+              </div>
+            </div>
+            <br>
+            <div class="container">
+              <div class="row">
+                <div class="col">
+                  <label for="recipient-name" class="col-form-label">Fecha de pago:</label>
+                  <input type="date" class="form-control" wire:model='fpago' id="recipient-name">
+                </div>
 
-  @isset($cuentas)
-  <div class="card shadow rounded">
-    <div class="accordion accordion-flush rounded" id="accordionFlushExample">
-      <div class="accordion-item" style="border-radius: 60px 60px 60px 60px;" >
-          <h2 class="accordion-header" style="border-radius: 60px 60px 60px 60px; color: #3a3e7b"  id="flush-headingOne">
-              <button class="accordion-button collapsed"  style="border-radius: 60px 60px 60px 60px; color: #3a3e7b"  type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseOne" aria-expanded="false" aria-controls="flush-collapseOne">
-                  INFORMACIÓN SOBRE PAGOS | Visualizar tabla
+                <div class="col">
+                  <label for="recipient-name" class="col-form-label"> Fecha del pago más reciente:</label>
+                  <input type="date" class="form-control" wire:model='pagor' id="recipient-name">
+                </div>
+              </div>
+            </div>
+            <br>
+            <div class="container">
+              <div class="row">
+                <div class="col">
+                  <label for="recipient-name" class="col-form-label">Monto de inscripción:</label>
+                  <input type="number" class="form-control" wire:model='montoins' id="recipient-name">
+                </div>
+                <div class="col">
+                  <label for="recipient-name" class="col-form-label">Monto mensual:</label>
+                  <input type="number" class="form-control" wire:model='montomen' id="recipient-name">
+                </div>
+                <div class="col">
+                  <label for="recipient-name" class="col-form-label">Monto de recuperación:</label>
+                  <input type="number" class="form-control" wire:model='montorecu' id="recipient-name">
+                </div>
+                <div class="col">
+                  <label for="recipient-name" class="col-form-label">Monto de descuento:</label>
+                  <input type="number" class="form-control" wire:model='montodes' id="recipient-name">
+                </div>
+              </div>
+            </div>
+            <br>
+            <button class="btn btn-pre2" wire:click="guardar_cuenta()">Agregar</button>    
+
+          </form>
+
+          <br>
+    @isset($cuentas)
+      <div class="accordion-item" style="border-radius: 60px 60px 60px 60px; border-color: #3a3e7b" >
+        <h2 class="accordion-header" style="border-radius: 60px 60px 60px 60px; color: #3a3e7b"  id="flush-headingOne">
+          <button class="accordion-button collapsed"  style="border-radius: 60px 60px 60px 60px; color: #3a3e7b"  type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseOne" aria-expanded="false" aria-controls="flush-collapseOne">
+            INFORMACIÓN SOBRE PAGOS | Visualizar tabla
               </button>
           </h2>    
 
@@ -89,43 +112,67 @@
                 <div class="table-responsive">
                     <table class="table table-light table-bordered">
                       <thead>
-                <tr>
-                    <th>NO.</th>
-                    <th>PRE-INSCRIPCIÓN</th>
-                    <th>NIVEL ACADÉMICO</th>
-                    <th>GRADO</th>
-                    <th>MES</th>
-                    <th>FECHA DE PAGO</th>
-                    <th>PAGO MÁS RECIENTE</th>
-                    <th>MONTO DE INSCRIPCIÓN</th>
-                    <th>MONTO DE RECUPERACIÓN</th>
-                    <th>MONTO MENSUAL</th>
-                    <th>MONTO DE DESCUENTO</th>
-                    <th>ESTADO</th>
-                </tr>
-            </thead>
-            <tbody>
-                @foreach ($cuentas as $cuenta)
-                    <tr>
-                        <td>{{$cuenta->ID_CUENTA}}</td>
-                        <td>{{$cuenta->ID_PRE}}</td>
-                        <td>{{$cuenta->ID_NVL}}</td>
-                        <td>{{$cuenta->ID_GR}}</td>
-                        <td>{{$cuenta->ID_MES}}</td>
-                        <td>{{$cuenta->FECHA_PAGO}}</td>
-                        <td>{{$cuenta->FECHA_ULIMOPAGO}}</td>
-                        <td>{{$cuenta->MONTO_INSCRIPCION}}</td>
-                        <td>{{$cuenta->MONTO_RECUPERACION}}</td>
-                        <td>{{$cuenta->MONTO_MENSUAL}}</td>
-                        <td>{{$cuenta->MONTO_DESCUENTO}}</td>
-                        @if($cuenta->ESTADO==1)
-                        <td>Activo</td>   
-                        @else
-                            <td>Inactivo</td>
-                        @endif
-                @endforeach
-            </tbody>
-        </table>
-    </div>
+                          <tr>
+                              <th>NO. GESTION</th>
+                              <th>PRE-INSCRIPCIÓN</th>
+                              <th>NIVEL ACADÉMICO</th>
+                              <th>GRADO</th>
+                              <th>MES</th>
+                              <th>FECHA DE PAGO</th>
+                              <th>PAGO MÁS RECIENTE</th>
+                              <th>MONTO DE INSCRIPCIÓN</th>
+                              <th>MONTO MENSUAL</th>
+                              <th>MONTO DE RECUPERACIÓN</th>
+                              <th>MONTO DE DESCUENTO</th>
+                              <th>ESTADO</th>
+                          </tr>
+                      </thead>
+                          <tbody>
+                            @foreach ($cuentas as $cuenta)
+                              <tr>
+                                @foreach($inscripciones as $inscrip)
+                                  @if($cuenta->ID_PRE==$inscrip->ID_PRE)
+                                    <td>#{{$inscrip->NO_GESTION}}</td>
+                                  @endif
+                                @endforeach
+                                @foreach($inscripciones as $inscrip)
+                                  @if($cuenta->ID_PRE==$inscrip->ID_PRE)
+                                    <td>{{$inscrip->NOMBRE_ES}}</td>
+                                  @endif
+                                @endforeach
+                                @foreach($academicos as $academico)
+                                  @if($cuenta->ID_NVL==$academico->ID_NVL)
+                                    <td>{{$academico->NIVEL_ACADEMICO}}</td>
+                                  @endif
+                                @endforeach
+                                @foreach($grados as $grado)
+                                  @if($cuenta->ID_GR==$grado->ID_GR)
+                                    <td>{{$grado->GRADO}}</td>
+                                  @endif
+                                @endforeach
+                                @foreach($meses as $mes)
+                                  @if($cuenta->ID_MES==$mes->ID_MES)
+                                    <td>{{$mes->DESCRIPCION}}</td>
+                                  @endif
+                                @endforeach
+                                <td>{{$cuenta->FECHA_PAGO}}</td>
+                                <td>{{$cuenta->FECHA_ULIMOPAGO}}</td>
+                                <td>{{$cuenta->MONTO_INSCRIPCION}}</td>
+                                <td>{{$cuenta->MONTO_RECUPERACION}}</td>
+                                <td>{{$cuenta->MONTO_MENSUAL}}</td>
+                                <td>{{$cuenta->MONTO_DESCUENTO}}</td>
+                                @if($cuenta->ESTADO==1)
+                                    <td>Activo</td>   
+                                  @else
+                                    <td>Inactivo</td>
+                                @endif
+                              </tr>
+                            @endforeach
+                          </tbody>
+                    </table>
+                </div>
+            </div>
+          </div>
+      </div>
 
     @endisset
