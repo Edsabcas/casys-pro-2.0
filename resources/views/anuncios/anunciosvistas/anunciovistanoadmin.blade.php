4a1d8057701d954c09b5c8e9b98b12c0d836fdae
Camila Guevara <camilaguevara277@gmail.com>
2022-07-17 17:05:27 -0600

    usuarios de alumnos en padres de familia

--- resources/views/anuncios/anunciosvistas/anunciovistanoadmin.blade.php
+++ resources/views/anuncios/anunciosvistas/anunciovistanoadmin.blade.php
@@ -223,8 +223,23 @@ $('#exampleModal1').modal('show');
 <!-- Encargado-->
 @if($rol_usuario==5)
 @foreach($alumnos_asignados as $alumnos)
-<div class="col">
-  <a href="/Vista_Alumno/{{$alumnos->ID_USER}}">{{$alumnos->NOMBRE}}</a>
+<div class="row">
+  <div class="col-xl-4 col-sm-7 col-13 mb-5">
+    <div class="card shadow rounded">
+      <div class="card-body">
+        <div class="text-center">
+          <div class="align-self-center">
+            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" style="color: #3a3e7b" class="bi bi-person-fill" viewBox="0 0 16 16">
+              <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z"/>
+            </svg>
+            <br>
+          <a href="/Vista_Alumno/{{$alumnos->ID_USER}}" style="color: #000000">{{$alumnos->NOMBRE}}</a>
+          </div>
+        </div>
+      <br>
+      </div>
+    </div>
+  </div>
 </div>
 
 @endforeach
