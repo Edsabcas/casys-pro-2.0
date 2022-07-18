9427d9dc540de46b5e0855db0c6232827faa6ef4
Jimena Gómez <jimenalejandra0292@gmail.com>
2022-07-17 22:44:07 -0600

    vistas padre de familia

---
+++ resources/views/alumnovista/pagos/panelpagos.blade.php
@@ -0,0 +1,57 @@
+<div class="card shadow rounded">
+    <div class="text-center">
+        <br>
+        <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="#a4cb39" class="bi bi-credit-card" viewBox="0 0 16 16">
+            <path d="M0 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V4zm2-1a1 1 0 0 0-1 1v1h14V4a1 1 0 0 0-1-1H2zm13 4H1v5a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V7z"/>
+            <path d="M2 10a1 1 0 0 1 1-1h1a1 1 0 0 1 1 1v1a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1v-1z"/>
+        </svg>
+        <br>
+        <br>
+        <h1 style="color: #3a3e7b"><strong>PAGOS</strong></h1>
+        <p style="color:black"><strong>Visualice los pagos realizados.</strong></p>
+        <br>
+    </div>
+</div>
+<br>
+<div class="card" style="border-color: #f8f9fc; background-color: #f8f9fc">
+    <div class="card-body">
+        <div class="row">
+            <div class="col-sm-4">
+                <div class="card" style="background-color: #ffffff">
+                    <div class="card-header text-light" style="background-color: #a4cb39"><strong>ENERO</strong></div>
+                    <div class="card-body">
+                        <h5 class="card-title"><strong>Monto a pagar: </strong> __________________</h5>
+                        <p class="card-text"><strong>Método de pago: </strong>_______________________</p>
+                        <p class="card-text"><strong>Observaciones: </strong>_________________________
+                            _________________________________________
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-4">
+                <div class="card" style="background-color: #ffffff">
+                    <div class="card-header text-light" style="background-color: #a4cb39"><strong>FEBRERO</strong></div>
+                    <div class="card-body">
+                        <h5 class="card-title"><strong>Monto a pagar: </strong> __________________</h5>
+                        <p class="card-text"><strong>Método de pago: </strong>_______________________</p>
+                        <p class="card-text"><strong>Observaciones: </strong>_________________________
+                            _________________________________________
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-4">
+                <div class="card" style="background-color: #ffffff">
+                    <div class="card-header text-light" style="background-color: #a4cb39"><strong>MARZO</strong></div>
+                    <div class="card-body">
+                        <h5 class="card-title"><strong>Monto a pagar: </strong> __________________</h5>
+                        <p class="card-text"><strong>Método de pago: </strong>_______________________</p>
+                        <p class="card-text"><strong>Observaciones: </strong>_________________________
+                            _________________________________________
+                        </p>
+                    </div>
+                </div>
+            </div>  
+        </div>
+    </div>
+</div>
---
+++ resources/views/alumnovista/reportes/panelreportes.blade.php
@@ -0,0 +1,135 @@
+<div class="card shadow rounded">
+    <div class="text-center">
+        <br>
+        <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="#a4cb39" class="bi bi-clipboard" viewBox="0 0 16 16">
+            <path d="M4 1.5H3a2 2 0 0 0-2 2V14a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V3.5a2 2 0 0 0-2-2h-1v1h1a1 1 0 0 1 1 1V14a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V3.5a1 1 0 0 1 1-1h1v-1z"/>
+            <path d="M9.5 1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-3a.5.5 0 0 1-.5-.5v-1a.5.5 0 0 1 .5-.5h3zm-3-1A1.5 1.5 0 0 0 5 1.5v1A1.5 1.5 0 0 0 6.5 4h3A1.5 1.5 0 0 0 11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3z"/>
+        </svg>
+        <br>
+        <br>
+        <h1 style="color: #3a3e7b"><strong>REPORTES</strong></h1>
+        <p style="color:black"><strong>Visualice los reportes.</strong></p>
+        <br>
+    </div>
+</div>
+<br>
+EJEMPLOS*
+<div class="card" style="border-color: #f8f9fc; background-color: #f8f9fc">
+    <div class="card-header text-center" style="background-color: #a4cb39">
+        <br>
+        <h2 style="color: #f8f9fc"><strong>REPORTES DE CONDUCTA</strong></h2>
+        <br>
+    </div>
+    <div class="card-body">
+        <div class="row">
+            <div class="col-sm-4">
+                <div class="card" style="background-color: #ffe366">
+                    <div class="card-header">Tarjeta Amarilla</div>
+                    <div class="card-body">
+                        <h5 class="card-title"><strong>Motivo: </strong>_________________________</h5>
+                        <p class="card-text"><strong>Observaciones: </strong>_________________________
+                            _________________________________________
+                        </p>
+                        <p class="card-text"><strong>Maestro(a): </strong>_____________________________</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-4">
+                <div class="card" style="background-color: #ff4040">
+                    <div class="card-header">Tarjeta Roja</div>
+                    <div class="card-body">
+                        <h5 class="card-title"><strong>Motivo: </strong>_________________________</h5>
+                        <p class="card-text"><strong>Observaciones: </strong>_________________________
+                            _________________________________________
+                        </p>
+                        <p class="card-text"><strong>Maestro(a): </strong>_____________________________</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-4">
+                <div class="card" style="background-color: #ffffff">
+                    <div class="card-header">Reporte</div>
+                    <div class="card-body">
+                        <h5 class="card-title"><strong>Motivo: </strong>_________________________</h5>
+                        <p class="card-text"><strong>Observaciones: </strong>_________________________
+                            _________________________________________
+                        </p>
+                        <p class="card-text"><strong>Maestro(a): </strong>_____________________________</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<br>
+<div class="card" style="border-color: #f8f9fc; background-color: #f8f9fc">
+    <div class="card-header text-center" style="background-color: #a4cb39">
+        <br>
+        <h2 style="color: #f8f9fc"><strong>REPORTES DE UNIDAD</strong></h2>
+        <br>
+    </div>
+    <div class="card-body">
+        <div class="row">
+            <div class="col-sm-4">
+                <div class="card" style="background-color: #ffffff">
+                    <div class="card-header">Reporte</div>
+                    <div class="card-body">
+                        <h5 class="card-title"><strong>PRIMERA UNIDAD</strong></h5>
+                        <p class="card-text"><strong>Pendiente: </strong>_____________________________</p>
+                        <p class="card-text"><strong>Observaciones: </strong>_________________________
+                            _________________________________________
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-4">
+                <div class="card" style="background-color: #ffffff">
+                    <div class="card-header">Reporte</div>
+                    <div class="card-body">
+                        <h5 class="card-title"><strong>SEGUNDA UNIDAD</strong></h5>
+                        <p class="card-text"><strong>Pendiente: </strong>_____________________________</p>
+                        <p class="card-text"><strong>Observaciones: </strong>_________________________
+                            _________________________________________
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-4">
+                <div class="card" style="background-color: #ffffff">
+                    <div class="card-header">Reporte</div>
+                    <div class="card-body">
+                        <h5 class="card-title"><strong>TERCERA UNIDAD</strong></h5>
+                        <p class="card-text"><strong>Pendiente: </strong>_____________________________</p>
+                        <p class="card-text"><strong>Observaciones: </strong>_________________________
+                            _________________________________________
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-4">
+                <div class="card" style="background-color: #ffffff">
+                    <div class="card-header">Reporte</div>
+                    <div class="card-body">
+                        <h5 class="card-title"><strong>CUARTA UNIDAD</strong></h5>
+                        <p class="card-text"><strong>Pendiente: </strong>_____________________________</p>
+                        <p class="card-text"><strong>Observaciones: </strong>_________________________
+                            _________________________________________
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-4">
+                <div class="card" style="background-color: #ffffff">
+                    <div class="card-header">Reporte</div>
+                    <div class="card-body">
+                        <h5 class="card-title"><strong>RECUPERACIÓN</strong></h5>
+                        <p class="card-text"><strong>Pendiente: </strong>_____________________________</p>
+                        <p class="card-text"><strong>Observaciones: </strong>_________________________
+                            _________________________________________
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
