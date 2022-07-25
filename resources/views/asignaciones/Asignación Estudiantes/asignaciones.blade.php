ab5ce01d46d79d0ef81390101a6e08c3beaf67cc
Camila Guevara <camilaguevara277@gmail.com>
2022-07-24 22:18:46 -0600

    cambios

--- resources/views/asignaciones/Asignación Estudiantes/asignaciones.blade.php
+++ resources/views/asignaciones/Asignación Estudiantes/asignaciones.blade.php
@@ -1,13 +1,75 @@
 <center>
-    <h2><strong><i>Asignación de secciones</i></strong></h2>
+    <h2 style="color:#a4cb39"><strong>Asignación de secciones</strong></h2>
     <br>
-      <a href="/Estudiantes_presencial" class="btn btn-pre2 ">Asignar modalidad presencial</a>
-    <a href="/Estudiantes_virtual" class="btn btn-pre2 ">Asignar modalidad virtual</a>
-</center>
-<br>
-<center>
-  <a href="/Estudiantes_presencial" class="btn btn-pre2 ">Ver alumnos asignados de forma presencial</a>
-    <a href="/Estudiantes_virtual" class="btn btn-pre2 ">Ver alumnos asignados de forma virtual</a>
-</center>
+
+    <div class="col">
+      <div class="col-xl-3 col-sm-8 col-10 mb-3">
+        <div class="card shadow rounded">
+          <div class="card-body">
+            <div class="text-center">
+              <div class="align-self-center">
+              </div>
+              <div class="text-center">
+                <a type="button">
+                  <p><a href="/Estudiantes_presencial" style="color: #3a3e7b"><strong>Asignar modalidad presencial</strong></a></p>
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="col">
+      <div class="col-xl-3 col-sm-8 col-10 mb-3">
+        <div class="card shadow rounded">
+          <div class="card-body">
+            <div class="text-center">
+              <div class="align-self-center">
+              </div>
+              <div class="text-center">
+                <a type="button">
+                  <p><a href="/Estudiantes_virtual" style="color: #3a3e7b"><strong>Asignar modalidad virtual</strong></a></p>
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="col">
+      <div class="col-xl-3 col-sm-8 col-10 mb-3">
+        <div class="card shadow rounded">
+          <div class="card-body">
+            <div class="text-center">
+              <div class="align-self-center">
+              </div>
+              <div class="text-center">
+                <a type="button">
+                  <p><a href="/Estudiantes_presencial" style="color: #3a3e7b"><strong>Ver alumnos asignados de forma presencial</strong></a></p>
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="col">
+      <div class="col-xl-3 col-sm-8 col-10 mb-3">
+        <div class="card shadow rounded">
+          <div class="card-body">
+            <div class="text-center">
+              <div class="align-self-center">
+              </div>
+              <div class="text-center">
+                <a type="button">
+                  <p><a href="/Estudiantes_virtual" style="color: #3a3e7b"><strong>Ver alumnos asignados de forma virtual</strong></a></p>
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
 
 
