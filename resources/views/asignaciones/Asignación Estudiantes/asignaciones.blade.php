724168d88d494c07a4cb4c72c1500ac921414398
unknown <ronia3606@gmail.com>
2022-07-24 19:13:42 -0600

    cambios

--- resources/views/asignaciones/Asignación Estudiantes/asignaciones.blade.php
+++ resources/views/asignaciones/Asignación Estudiantes/asignaciones.blade.php
@@ -1,8 +1,13 @@
 <center>
     <h2><strong><i>Asignación de secciones</i></strong></h2>
     <br>
-    <a href="/Estudiantes_presencial" class="btn btn-pre2 ">Asignar modalidad presencial</a>
+      <a href="/Estudiantes_presencial" class="btn btn-pre2 ">Asignar modalidad presencial</a>
     <a href="/Estudiantes_virtual" class="btn btn-pre2 ">Asignar modalidad virtual</a>
+</center>
+<br>
+<center>
+  <a href="/Estudiantes_presencial" class="btn btn-pre2 ">Ver alumnos asignados de forma presencial</a>
+    <a href="/Estudiantes_virtual" class="btn btn-pre2 ">Ver alumnos asignados de forma virtual</a>
 </center>
 
 
